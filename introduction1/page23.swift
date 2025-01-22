534e9e431411a27fef652044a70e67ed82a1f006
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-22 21:08:40 +0100

    Create page23.swift

---
+++ introduction1/page23.swift
@@ -0,0 +1,18 @@
+func yourFuncName() {
+  moveForward()
+  collectGem()
+  moveForward()
+  toggleSwitch()
+}
+
+yourFuncName()
+moveForward()
+turnLeft()
+yourFuncName()
+moveForward()
+moveForward()
+turnLeft()
+yourFuncName()
+moveForward()
+turnLeft()
+yourFuncName()
